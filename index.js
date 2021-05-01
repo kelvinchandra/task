9755744f6b7fa5bf9c53681061e520ea6e39d715
kelvin <kelvin.salim@futuready.com>
2021-05-01 16:24:59 +0700

    task2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-async function main(){
+async function task1(){
     const response = axios
     .get(
       "https://gist.githubusercontent.com/Jekiwijaya/c72c2de532203965bf818e5a4e5e43e3/raw/2631344d08b044a4b833caeab8a42486b87cc19a/gistfile1.txt"
@@ -34,4 +34,22 @@ async function main(){
     });
 } 
 
-main()
+
+async function task2(){
+    const response = axios
+    .get(
+      "https://gist.githubusercontent.com/Jekiwijaya/0b85de3b9ff551a879896dd78256e9b8/raw/e9d58da5d4df913ad62e6e8dd83c936090ee6ef4/gistfile1.txt"
+    )
+    .then(function (response) {
+        const data = response.data;
+        const mySet1 = new Set(data);
+        let result = '';
+        for(const value of mySet1.values()){
+            result = result + value
+            console.log(`result in progress: ${result}`)
+        }
+        console.log(`result ${result}`)
+    });
+} 
+
+task2()
